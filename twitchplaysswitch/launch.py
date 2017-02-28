59602ddbdd1f264d15338bbe4d2836c707694697
dekunukem <dekunukem@gmail.com>
2017-02-28 16:25:10 +0000

    tps working

--- twitchplaysswitch/launch.py
+++ twitchplaysswitch/launch.py
@@ -1,140 +1,65 @@
-import os
 import sys
 import time
 import serial
-import random
-# import switch_ctrl
+import threading
+import switch_ctrl
 import irc_bot_noblock
 from command_printer import *
 from collections import OrderedDict
 
-def current_time_sec():
-    return int(time.time())
+switch_cmd_map = {
+"dup" : [0, "du"],
+"ddown" : [1, "dd"],
+"dleft" : [2, "dl"],
+"dright" : [3, "dr"],
+"l" : [4, "ls"],
+"zl" : [5, "zl"],
+"minus" : [6, "-"],
+"cap" : [7, "cap"],
+"lsl" : [8, "lsl"],
+"lsr" : [9, "lsr"],
+"syncl" : [10, "syncl"],
+"sbl" : [11, "sbl"],
+"a" : [12, "a"],
+"b" : [13, "b"],
+"x" : [14, "x"],
+"y" : [15, "y"],
+"r" : [16, "rs"],
+"zr" : [17, "zr"],
+"plus" : [18, "+"],
+"h" : [19, "h"],
+"rsl" : [20, "rsl"],
+"rsr" : [21, "rsr"],
+"syncr" : [22, "syncr"],
+"sbr" : [23, "sbr"],
+"up" : [24, ""],
+"down" : [25, ""],
+"left" : [26, ""],
+"right" : [27, ""],
+"cup" : [28, ""],
+"cdown" : [29, ""],
+"cleft" : [30, ""],
+"cright" : [31, ""]
+}
 
-def current_time_float():
-    return time.time()
-
-main_directional_control = "dpad"
-buttonpress_delay = 0.2
-
+latest_cmd = None
 username_color_dict = OrderedDict()
-cpad_up = 'ch 140 255\n'
-cpad_down = 'ch 140 0\n'
-cpad_left = 'ch 255 140\n'
-cpad_right = 'ch 0 140\n'
-dpad_up = 'bh u\n'
-dpad_down = 'bh d\n'
-dpad_left = 'bh l\n'
-dpad_right = 'bh r\n'
 
 def is_valid_command(command_list):
     if len(command_list) > 3 or len(command_list) <= 0:
         return False
-
-    if("up" in command_list and "down" in command_list) or\
-    ("left" in command_list and "right" in command_list) or\
-    ("a" in command_list and "b" in command_list) or\
-    ("x" in command_list and "y" in command_list) or\
-    ("l" in command_list and "r" in command_list) or\
-    ("start" in command_list and "select" in command_list):
-        return False
     return True
 
-def roll_random(probability):
-    if(probability >= 1):
-        return True
-    if(probability <= 0):
-        return False
-    return random.random() < probability
-
-def press(chat_username, command_list, admin_override = False, touch_delay = 0.1):
-    global buttonpress_delay
-    global next_release
-
-    try:
-        for item in command_list:
-            if button_enabled or admin_override:
-                if item == "a" :
-                    ser.write(('bh a\n').encode())
-                elif item == "b" :
-                    ser.write(('bh b\n').encode())
-                elif item == "x" :
-                    ser.write(('bh x\n').encode())
-                elif item == "y" :
-                    ser.write(('bh y\n').encode())
-
-                elif main_directional_control == "cpad":
-                    if item == "up":
-                        ser.write(cpad_up.encode())
-                    elif item == "down":
-                        ser.write(cpad_down.encode())
-                    elif item == "left":
-                        ser.write(cpad_left.encode())
-                    elif item == "right":
-                        ser.write(cpad_right.encode())
-                    elif item == "dup":
-                        ser.write(dpad_up.encode())
-                    elif item == "ddown":
-                        ser.write(dpad_down.encode())
-                    elif item == "dleft":
-                        ser.write(dpad_left.encode())
-                    elif item == "dright":
-                        ser.write(dpad_right.encode())
-
-                elif main_directional_control == "dpad":
-                    if item == "up":
-                        ser.write(dpad_up.encode())
-                    elif item == "down":
-                        ser.write(dpad_down.encode())
-                    elif item == "left":
-                        ser.write(dpad_left.encode())
-                    elif item == "right":
-                        ser.write(dpad_right.encode())
-                    elif item == "cup":
-                        ser.write(cpad_up.encode())
-                    elif item == "cdown":
-                        ser.write(cpad_down.encode())
-                    elif item == "cleft":
-                        ser.write(cpad_left.encode())
-                    elif item == "cright":
-                        ser.write(cpad_right.encode())
-
-                elif item == "l":
-                    ser.write(('bh ls\n').encode())
-                elif item == "r":
-                    ser.write(('bh rs\n').encode())
-                elif item == "start":
-                    ser.write(('bh st\n').encode())
-                elif item == "select":
-                    ser.write(('bh se\n').encode())
-                elif item == "home":
-                    ser.write(('bh h\n').encode())
-
-        for item in command_list:
-            if touch_enabled or admin_override:
-                coord_list = str.split(item, ',')
-                if len(coord_list) == 2 :
-                    x = int(coord_list[0])
-                    y = int(coord_list[1])
-                    if touch_delay > 0.1:
-                        touch_delay = 0.1
-                    delay_cmd = "td " + str(int(touch_delay * 1000)) + "\n"
-                    if(touch_delay != 0.1):
-                        print("=====new touchscreen delay: " + str(delay_cmd))
-                    cmd = "tc " + str(x) + " " + str(y) + "\n"
-                    ser.write(delay_cmd.encode())
-                    ser.write(cmd.encode())
-                    overlay_x = (x/320)*crosshair_overlay_width
-                    overlay_y = (y/240)*crosshair_overlay_height
-                    canvas.create_image(overlay_x, overlay_y, image = crosshair)
-                    canvas.update()
-                    touch_crosshair_root.update()
-    except:
-        reset_serial()
-    next_release = current_time_float() + buttonpress_delay
+def execute_cmd(cmd_list):
+    global latest_cmd
+    button_args = []
+    for item in cmd_list:
+        if 0 <= switch_cmd_map[item][0] <= 23:
+            button_args.append(switch_cmd_map[item][1])
+    latest_cmd = (switch.button_click, button_args)
+    return
 
 def do_anarchy(chat_username, cmd_list):
-    global next_release
     if(is_valid_command(cmd_list)):
         command_printer_in = []
         command_printer_in.append(chat_username)
@@ -144,17 +69,7 @@ def do_anarchy(chat_username, cmd_list):
         else:
             command_printer_in.append("white")
         print_command_list(command_printer_in, 20, 0)
-        # diff = buttonpress_delay - (next_release - current_time_float())
-        # td = 0.0
-        # # diff is how long until the schduled release
-        # print(diff)
-        # if diff < 0.2:
-        #   print("======early release: " + str(diff) + "======")
-        #   release_control()
-        #   td = diff
-        # else:
-        #   td = 0.1
-        # press(chat_username, cmd_list, touch_delay = td)
+        execute_cmd(cmd_list)
 
 def safe_print(item):
     try:
@@ -162,30 +77,68 @@ def safe_print(item):
     except Exception:
         print(item.encode('utf-8'))
 
-if(len(sys.argv) != 2):
-    print ('launch.py <serial port>')
-    sys.exit(0)
+def get_cmd(chat_line):
+    try:
+        line_split = chat_line.lower().split("+")
+        cmd_list = []
+        cmd_list_nospace = []
+        for item in line_split:
+            cmd_list.append(item)
+        if len(cmd_list) <= 1:
+            cmd_list_nospace = cmd_list
+        else:
+            for item in cmd_list[:-1]:
+                cmd_list_nospace.append(item.replace(" ", ''))
+        cmd_list_nospace.append(cmd_list[-1])
+        cmd_list_nospace[-1] = cmd_list_nospace[-1].lstrip().split(" ")[0]
+        for item in cmd_list_nospace:
+            if item not in switch_cmd_map:
+                return []
+        return list(set(cmd_list_nospace))
+    except Exception as e:
+        safe_print("get_cmd: " + str(e))
+        return []
 
-nickname = 'twitch_plays_3ds'
-oauth = ''
+def worker():
+    global latest_cmd
+    while 1:
+        if latest_cmd != None:
+            func = latest_cmd[0]
+            arg = latest_cmd[1]
+            func(200, arg)
+            latest_cmd = None
+        time.sleep(0.005)
+
+# if(len(sys.argv) != 2):
+#     print ('launch.py <serial port>')
+#     sys.exit(0)
+
+switch = switch_ctrl.switch_ctrl("COM4", "COM9")
+switch.connect()
+
+nickname = 'STM32F429ZIT6U'
+oauth = 'oauth:xj328fatmerr48qqlmkh59onpp6qzc'
 chat_channel = 'twitchplayspokemon'
 chat_server = ['irc.chat.twitch.tv', 6667]
 bot = irc_bot_noblock.irc_bot(nickname, oauth, chat_channel, chat_server[0], chat_server[1], timeout=300, tags = 1)
 bot.connect()
+
+t = threading.Thread(target=worker)
+t.start()
 
 while 1:
     tmi_list = bot.get_parsed_message()
     tmi_list.reverse()
     for item in [x for x in tmi_list if "." not in x.username]:
         username = item.username
-        message_orig = item.message.replace(chr(1) + "ACTION", "/me").replace(chr(1), '').lstrip().rstrip()
+        message_orig = item.message.replace(chr(1) + "ACTION", "").replace(chr(1), '').lstrip().rstrip()
         safe_print(item.username + ": " + message_orig)
 
         username_color_dict[username] = item.color
-        if len(username_color_dict) > 50:
+        if len(username_color_dict) > 200:
             username_color_dict.popitem(last=False)
 
-        message_split_list = list(set(message_orig.replace(" ", "").lower().split("+")))
-        do_anarchy(username, message_split_list)
+        command_list = get_cmd(message_orig)
+        do_anarchy(username, command_list)
     user_command_root.update()
     time.sleep(0.01)
