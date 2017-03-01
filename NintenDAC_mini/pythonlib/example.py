790407349d32eb058ad0748ae9924c6309510f80
dekunukem <dekunukem@gmail.com>
2017-03-01 21:18:01 +0000

    update readme

--- NintenDAC_mini/pythonlib/example.py
+++ NintenDAC_mini/pythonlib/example.py
@@ -1,17 +1,36 @@
-import time
 import ndacmini
 
-def sleep_ms(delay_ms):
-    time.sleep(float(delay_ms)/1000)
-
-switch = ndacmini.ndacmini("COM4", "COM8")
+switch = ndacmini.ndacmini("COM5", "COM6")
 switch.connect()
 
-try:
-	while 1:
-		switch.button_click(100, ["du", "ls", "zl", "rs", "zr", "+"])
-		# switch.stick_nudge('l', 100, 45, 67)
-		# switch.stick_hold('l', 45, 67)
-		# switch.button_stick_ctrl(100, 67, ["du", "ls", "zl", "rs", "zr", "+"], (23,45), (67,89))
-except KeyboardInterrupt:
-	switch.disconnect()
+# give it a list of valid button args
+switch.button_hold(['a', 'b', 'zl', 'zr'])
+switch.button_release(['du'])
+switch.button_release_all()
+
+# press down button(s) then release it
+# first arg is how long to press the button(s) down
+# second arg is how long to release them before the next action
+# value smaller than 1 frame (33ms) probably won't get recognized by the console
+switch.button_click(67, 67, ['+', '-', 'h'])
+
+# first arg is which stick to hold, should be 'l' or 'r'
+# second and third arg are desired stick positions
+# must between 0 to 255 inclusive, 127 is netural postion
+switch.stick_hold('l', 64, 192)
+switch.stick_release('r')
+
+# give back stick control to user
+switch.stick_disengage('l')
+
+# hold the stick to a position then release it
+# first arg is which side, should be 'l' or 'r'
+# second arg is how long to hold the stick
+# third arg is how long to release the stick before the next action
+# fourth and fifth arg are stick position x and y
+switch.stick_nudge('l', 67, 67, 192, 168)
+
+# release all buttons and disengage all sticks
+switch.reset()
+
+switch.disconnect()
